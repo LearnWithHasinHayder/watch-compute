e114410997357ecc13779ddc0089897c17bc2fb9
Hasin Hayder <countdraculla@gmail.com>
2023-10-05 21:38:21 +0600

    Computed vs Function

--- src/components/Fruits.vue
+++ src/components/Fruits.vue
@@ -8,18 +8,9 @@ const fruitsWithPrice = ref([
     { name: 'Pineapple', price: 2.10 },
 ]);
 
-const fruitName = ref('');
-const fruitPrice = ref(0);
-const add = ()=>{
-    fruitsWithPrice.value.push({
-        name: fruitName.value,
-        price: fruitPrice.value
-    });
-    fruitName.value = '';
-    fruitPrice.value = 0;
-}
 
 const totalPrice = computed(()=>{
+    console.log("Computed")
     let total = 0;
     for(let i = 0; i < fruitsWithPrice.value.length; i++){
         total += fruitsWithPrice.value[i].price*1;
@@ -27,25 +18,27 @@ const totalPrice = computed(()=>{
     return total.toFixed(2);
 })
 
-// let totalPrice = ref(0);
-// for(let i = 0; i < fruitsWithPrice.value.length; i++){
-//     totalPrice.value += fruitsWithPrice.value[i].price;
-// }
-// totalPrice.value = totalPrice.value.toFixed(2);
+const totalPrice2 = function(){
+    console.log("Function")
+    let total = 0;
+    for(let i = 0; i < fruitsWithPrice.value.length; i++){
+        total += fruitsWithPrice.value[i].price*1;
+    }
+    return total.toFixed(2);
+}
+
 
 </script>
  
 <template>
-    <p class="mb-10">{{ fruitsWithPrice }}</p>
-    <p>Fruit Name: <input  v-model='fruitName'/></p>
-    <p>Fruit Price: <input  v-model='fruitPrice'/></p>
-    <!-- <p>Fruit Price: <input type="number" v-model='fruitPrice'/></p> -->
-    <p> Total: {{ totalPrice }}  </p>
-    <p class="mt-5">
-        <button @click="add()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Add Fruit
-        </button>
-    </p>
+
+    <p> C Total: {{ totalPrice }}  </p>
+    <p> C Total: {{ totalPrice }}  </p>
+    <p> C Total: {{ totalPrice }}  </p>
+    <p> D Total: {{ totalPrice2() }}  </p>
+    <p> D Total: {{ totalPrice2() }}  </p>
+    <p> D Total: {{ totalPrice2() }}  </p>
+
 </template>
  
 <style scoped></style>
