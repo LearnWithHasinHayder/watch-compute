d02c0a1e96aa19f9d4db8524a75c1a51d8e386b6
Hasin Hayder <countdraculla@gmail.com>
2023-10-05 21:18:22 +0600

    Episode 01

---
+++ src/components/Fruits.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref, computed } from 'vue';
+const fruitsWithPrice = [
+    { name: 'Apple', price: 1.20 },
+    { name: 'Orange', price: 1.40 },
+    { name: 'Banana', price: 1.60 },
+    { name: 'Mango', price: 1.80 },
+    { name: 'Pineapple', price: 2.10 },
+];
+
+
+
+const index = ref(0);
+// const currentFruit = ref(fruitsWithPrice[index.value]);
+
+const currentFruit = computed(() => fruitsWithPrice[index.value]);
+// const currentFruit = computed(() => {
+//     return fruitsWithPrice[index.value]
+// });
+
+
+const next = () => {
+    index.value = (index.value + 1) % fruitsWithPrice.length;
+};
+</script>
+ 
+<template>
+
+    <p class="mb-2">
+        Index: {{ index }}
+    </p>
+    <p class="mb-2">
+        Fruit: {{ currentFruit.name }}
+    </p>
+    <p class="mb-2">
+        Price: {{ currentFruit.price }}
+    </p>
+    <p class="mt-5">
+        <button @click="next()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Next Fruit
+        </button>
+    </p>
+</template>
+ 
+<style scoped></style>
