11ec77fe6946529fe49ef33303df59801f6c35e4
Hasin Hayder <countdraculla@gmail.com>
2023-10-05 22:13:55 +0600

    watchEffect

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,49 +1,46 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, reactive, watch, watchEffect } from 'vue';
 const postId = ref(1);
-
-const fruitsWithPrice = ref([
-    { name: 'Apple', price: 1.20 },
-    { name: 'Orange', price: 1.40 },
-    { name: 'Banana', price: 1.60 },
-    { name: 'Mango', price: 1.80 },
-    { name: 'Pineapple', price: 2.10 },
-]);
-
-const totalPrice = ref(0)
-watch(fruitsWithPrice,(newVal, oldVal) => {
-    let total = 0;
-    for (let i = 0; i < newVal.length; i++) {
-        total += newVal[i].price * 1;
-    }
-    console.log(total);
-    totalPrice.value = total.toFixed(2);
-}, {deep:true, immediate:true});
-
-const data = ref("Post Id is " + postId.value);
-
-watch(postId, (newVal, oldVal)=>{
-    data.value = "Post Id is " + newVal;
+const post = reactive({
+    title: '',
 })
+
+watchEffect(async () => {
+    post.title = "Loading...";
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId.value}`);
+    const json = await response.json();
+    post.title = json.title;
+})
+
+
+//async watch
+watch(postId, async (newVal, oldVal)=>{
+    post.title = "Loading...";
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${newVal}`);
+    const json = await response.json();
+    post.title = json.title;
+},{immediate: true})
+
+// watch(postId, (newVal, oldVal)=>{
+//     post.title = "Loading...";
+//     fetch(`https://jsonplaceholder.typicode.com/posts/${newVal}`)
+//     .then(response => response.json())
+//     .then(json => {
+//         post.title = json.title;
+//     })
+// },{immediate: true})
 </script>
  
 <template>
-    <p> {{ fruitsWithPrice }}</p>
-    <p> {{ totalPrice }} </p>
-    <p>{{ data }}</p>
-<p> 
-    Post ID: {{ postId }}
-</p>
-<p>
-    <input type="text" v-model="postId">
-</p>
-<p>
-    <button @click="fruitsWithPrice.pop()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Button
-    </button>
-</p>
+    <p>
+        Post ID: {{ postId }}
+    </p>
+    <p>
+        Post Title: {{ post.title }}
+    </p>
+    <p class="mt-10">
+        <input type="text" v-model="postId">
+    </p>
 </template>
  
-<style scoped>
- 
-</style>
+<style scoped></style>
